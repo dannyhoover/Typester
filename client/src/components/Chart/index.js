63592e11e4744f0cb62aa4719719ef044e0b9f29
Clyde Clark <clydeclark78@gmail.com>
2020-12-17 19:27:35 -0800

    added gradient to styling

--- client/src/components/Chart/index.js
+++ client/src/components/Chart/index.js
@@ -4,24 +4,28 @@ import { Line } from "react-chartjs-2";
 function Chart(props) {
   console.log(props.data.chartData);
   return (
-    <div className="chart">
-      {props.data.chartData != undefined ? (
-        <Line
-          data={props.data.chartData}
-          width={100}
-          height={50}
-          options={{
-            title: {
-              display: true,
-              text: "Words per Minute History",
-              fontSize: 25,
-            },
-            maintainAspectRatio: true,
-          }}
-        />
-      ) : (
-        <div>No results to display</div>
-      )}
+    <div>
+      <div className="head-gradient"></div>
+        <div className="chart">
+          {props.data.chartData != undefined ? (
+            <Line
+              data={props.data.chartData}
+              width={100}
+              height={50}
+              options={{
+                title: {
+                  display: true,
+                  text: "Words per Minute History",
+                  fontSize: 25,
+                },
+                maintainAspectRatio: true,
+              }}
+            />
+          ) : (
+            <div>No results to display</div>
+          )}
+        </div>
+    <div className="foot-gradient"></div>
     </div>
   );
 }
