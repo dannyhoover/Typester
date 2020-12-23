d3f80b52eaf3d3e36e74b47a32e3d1fc01656f7d
Isac Pederson <isacjped@gmail.com>
2020-12-22 19:10:54 -0800

    signup page

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,39 +1,41 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import LoginLogoutButton from "../LoginLogoutButton";
+import SignupPage from "../SignupPage";
 
-const Header = ({children}) => (
-    <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">
-                Typster
-            </Link>
+const Header = ({ children }) => (
+  <header>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Typster
+      </Link>
 
-            <button
-                type="button"
-                className="navbar-toggler"
-                data-toggle="collapse"
-                data-target="#navbar-content"
-                aria-controls="navbar-content"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
+      <button
+        type="button"
+        className="navbar-toggler"
+        data-toggle="collapse"
+        data-target="#navbar-content"
+        aria-controls="navbar-content"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-            <div className="collapse navbar-collapse" id="navbar-content">
-                <ul className="navbar-nav ml-auto">
-                    {(!Array.isArray(children) ? [children] : children).map(
-                        (child, i) => (
-                            <li key={i} className="nav-item">
-                                {child}
-                            </li>
-                        )
-                    )}
-                </ul>
-                <LoginLogoutButton />
-            </div>
-        </nav>
-    </header>
+      <div className="collapse navbar-collapse" id="navbar-content">
+        <ul className="navbar-nav ml-auto">
+          {(!Array.isArray(children) ? [children] : children).map(
+            (child, i) => (
+              <li key={i} className="nav-item">
+                {child}
+              </li>
+            )
+          )}
+        </ul>
+        <LoginLogoutButton />
+        <SignupPage />
+      </div>
+    </nav>
+  </header>
 );
 
 export default Header;
